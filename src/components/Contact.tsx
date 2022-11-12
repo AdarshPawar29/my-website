1aa3e716dfc7ef8c891cd176378ebf995781ccb1
Adarsh Pawar <adarshpawar@Adarshs-MacBook-Pro.local>
2022-11-12 16:20:56 +0530

    added scss

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -58,96 +58,98 @@ const Contact = (props: Props) => {
     },
   });
   return (
-    <div className="contact">
-      <div className="contact-form">
-        <div className="html">{"<html>"}</div>
-        <div className="body">{"<body>"}</div>
-        <div>
-          <div className="h2">{"<h2>"}</div>
-          <div className="contact-me">
-            <h2>Contact Me</h2>
+    <>
+      <div className="contact">
+        <div className="contact-form">
+          <div className="html">{"<html>"}</div>
+          <div className="body">{"<body>"}</div>
+          <div>
+            <div className="h2">{"<h2>"}</div>
+            <div className="contact-me">
+              <h2>Contact Me</h2>
+            </div>
+            <div className="h2">{"</h2>"}</div>
           </div>
-          <div className="h2">{"</h2>"}</div>
-        </div>
-        <div className="my-info-para">
-          <div className="p">{"<p>"}</div>
-          <div className="contact-me-info">
-            <p>
-              I’m interested in development opportunities – especially ambitious
-              or large projects. However, if you have other request or question,
-              don’t hesitate to use the form.
-            </p>
+          <div className="my-info-para">
+            <div className="p">{"<p>"}</div>
+            <div className="contact-me-info">
+              <p>
+                I’m interested in development opportunities – especially
+                ambitious or large projects. However, if you have other request
+                or question, don’t hesitate to use the form.
+              </p>
+            </div>
+            <div className="p">{"</p>"}</div>
           </div>
-          <div className="p">{"</p>"}</div>
-        </div>
-        <div className="form">{"<form>"}</div>
-        <div className="form-fields">
-          <ThemeProvider theme={theme}>
-            <form ref={form} onSubmit={sendEmail}>
-              <div className="form-fields-inputs">
-                <div className="form-fields-inputs-name-email">
+          <div className="form">{"<form>"}</div>
+          <div className="form-fields">
+            <ThemeProvider theme={theme}>
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="form-fields-inputs">
+                  <div className="form-fields-inputs-name-email">
+                    <TextField
+                      id="standard-basic"
+                      label="Name"
+                      variant="standard"
+                      color="primary"
+                      name="from_name"
+                      fullWidth
+                    />
+                    <TextField
+                      id="standard-basic"
+                      label="Email"
+                      variant="standard"
+                      color="primary"
+                      name="email"
+                      fullWidth
+                    />
+                  </div>
                   <TextField
                     id="standard-basic"
-                    label="Name"
+                    label="Subject"
                     variant="standard"
                     color="primary"
-                    name="from_name"
+                    name="subject"
                     fullWidth
                   />
                   <TextField
                     id="standard-basic"
-                    label="Email"
+                    label="Message"
+                    multiline
+                    rows={4}
                     variant="standard"
-                    color="primary"
-                    name="email"
+                    name="message"
                     fullWidth
-                  />
-                </div>
-                <TextField
-                  id="standard-basic"
-                  label="Subject"
-                  variant="standard"
-                  color="primary"
-                  name="subject"
-                  fullWidth
-                />
-                <TextField
-                  id="standard-basic"
-                  label="Message"
-                  multiline
-                  rows={4}
-                  variant="standard"
-                  name="message"
-                  fullWidth
-                  color="primary"
-                />
-                <div className="submit-btn">
-                  <Button
-                    variant="outlined"
                     color="primary"
-                    type="submit"
-                    onClick={handleClick}
-                  >
-                    Submit
-                  </Button>
+                  />
+                  <div className="submit-btn">
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      type="submit"
+                      onClick={handleClick}
+                    >
+                      Submit
+                    </Button>
+                  </div>
                 </div>
-              </div>
-            </form>
-          </ThemeProvider>
+              </form>
+            </ThemeProvider>
+          </div>
+          <div className="form">{"</form>"}</div>
+          <div className="body">{"</body>"}</div>
+          <div className="html">{"</html>"}</div>
         </div>
-        <div className="form">{"</form>"}</div>
-        <div className="body">{"</body>"}</div>
-        <div className="html">{"</html>"}</div>
-      </div>
-      <div className="contact-map">
+        {/* <div className="contact-map">
         <Map />
+      </div> */}
       </div>
       <MessageHub
         children={(add: AddFunction) => {
           ref.current = add;
         }}
       />
-    </div>
+    </>
   );
 };
 
